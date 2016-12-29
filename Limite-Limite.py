4c8dd10472b66ee2dc604d5bde32feb1873a646c
leaxs <axelport@hotmail.fr>
2016-12-29 11:51:43 +0100

    Add comment to the code

--- Limite-Limite.py
+++ Limite-Limite.py
@@ -3,70 +3,38 @@ from tkinter.messagebox import *
 from tkinter.simpledialog import *
 import codecs
 import random as rd
-import time
 import os 
 
 ##Start screen
-
+#Add player to the party
 class addingPlayer:
     def __init__(self,players_list):
         self.players_list = players_list
     def __call__(self):
         name = askstring("Adding player","Player name?")
         (self.players_list).insert(END,name)
-
+        
+#Remove the selected player in listbox
 class removingPlayer:
     def __init__(self,players_list):
         self.players_list = players_list
     def __call__(self):
         if(askokcancel("Removing player", "Would you really remove "+(self.players_list).get(ACTIVE)+" from the party?")):
             (self.players_list).delete(ACTIVE)
-
-def getCardInDirectory(carddeck,panel):
-    card_dir = os.listdir('Carddeck') 
-    for i in card_dir:
-        var = BooleanVar()
-        checkbox = Checkbutton(panel,variable=var,text=i,onvalue=True, offvalue=False)
-        checkbox.pack()
-        carddeck.append((i,var))
-
-def startScreen():
-    #Deck choose
-    available_deck_panel = LabelFrame(root, text="Choose your deck")
-    available_deck_panel.pack(fill="both", expand="yes")
-    
-    carddeck = []
-    getCardInDirectory(carddeck,available_deck_panel)
-    
-    #player management
-    
-    players_panel = LabelFrame(root, text="Players")
-    players_panel.pack(fill="both", expand="yes")
-    
-    players_list = Listbox(players_panel)
-    players_list.pack()
-    
-    buttonframe = Frame(players_panel, borderwidth=2, relief=FLAT)
-    buttonframe.pack()
-    addPlayer=Button(buttonframe, text="Add", command=addingPlayer(players_list))
-    addPlayer.pack(side=LEFT)
-    removePlayer=Button(buttonframe, text="Remove", command=removingPlayer(players_list))
-    removePlayer.pack(side=RIGHT)
-    
-    play=Button(root, text="Start", command=StartGame(carddeck,players_list))
-    play.pack()
-    
-
-##GAME
+            
+#Start button manager
 class StartGame:
     def __init__(self,detectedDeck,players):
         self.players_list = players
         self.carddeck = detectedDeck
     def __call__(self):
+        #Check if there is enought players (minimum is 3)
         players = (self.players_list).get(0,END)
-        if(len(players)<=1):
+        if(len(players)<=2):
             showerror("","Not enought player!")
             return
+            
+        #Getting black and white cards
         white_cards = []
         black_cards = []
         for i in self.carddeck:
@@ -82,57 +50,113 @@ class StartGame:
                     else:
                         white_cards.append(line[:-2])
                 f.close()
-
+                
+        #Check if there is at least one black and one white cards
+        if(len(black_cards) == 0 or len(white_cards)==0):
+            showerror("","You should select decks to have both blacks and whites cards!")
+            return
+        
+        #Lauch the game
         mainGameLoop(players,white_cards,black_cards)
         
+#Research all files in carddeck directory and return their name
+def getCardInDirectory(carddeck,panel):
+    card_dir = os.listdir('Carddeck') 
+    for i in card_dir:
+        var = BooleanVar()
+        checkbox = Checkbutton(panel,variable=var,text=i,onvalue=True, offvalue=False)
+        checkbox.pack()
+        carddeck.append((i,var))
+
+#Title screen that allows to choose used deck and add/remove player
+def startScreen():
+    #Deck management
+    available_deck_panel = LabelFrame(root, text="Choose your deck")
+    available_deck_panel.pack(fill="both", expand="yes")
+    
+    carddeck = []
+    getCardInDirectory(carddeck,available_deck_panel)
+    
+    #Player management
+    players_panel = LabelFrame(root, text="Players")
+    players_panel.pack(fill="both", expand="yes")
+    
+    players_list = Listbox(players_panel)
+    players_list.pack()
+    
+    buttonframe = Frame(players_panel, borderwidth=2, relief=FLAT)
+    buttonframe.pack()
+    addPlayer=Button(buttonframe, text="Add", command=addingPlayer(players_list))
+    addPlayer.pack(side=LEFT)
+    removePlayer=Button(buttonframe, text="Remove", command=removingPlayer(players_list))
+    removePlayer.pack(side=RIGHT)
+    
+    #Start game button
+    play=Button(root, text="Start", command=StartGame(carddeck,players_list))
+    play.pack()
+    
+##GAME
+#Class to hold player information
 class Player:
+    #Select button manager
     class selectCard:
         def __init__(self,player):
             self.player = player
         def __call__(self):
+            #Get the current number of choosed card
             indexOfEmptyCard = -1
             for i in range(3):
                 if(len((self.player).selectedCard[i].get()) == 0):
                     indexOfEmptyCard = i
                     break
             
+            #If the player try to choose more cards thatthe black card require, it shows an error message and stop the process
             if(indexOfEmptyCard>(self.player).whiteCardNumber or indexOfEmptyCard==-1):
                 showerror("","You should only pick "+str((self.player).whiteCardNumber)+" cards!")
                 return
-                
+            
+            #Remove the card from players deck and put it in the selected card container
             ((self.player).selectedCard[indexOfEmptyCard]).set(((self.player).card).get(ACTIVE))
             ((self.player).card).delete(ACTIVE)
 
+    #Unselect button manager
     class unselectCard:
         def __init__(self,player):
             self.player = player
         def __call__(self):
+            #Get the index of the last selected card
             indexOfLastCard = -1
             for i in range(3):
                 if(len((self.player).selectedCard[i].get()) != 0):
                     indexOfLastCard = i
             
+            #If theres is no cards, it shows an error message and stop the process
             if(indexOfLastCard==-1):
                 showerror("","No card to unselect!")
                 return
-                
-            print((self.player).selectedCard[indexOfLastCard].get())
+            
+            #Remove the card from selected card container and put it in the players deck
             ((self.player).card).insert(END,(self.player).selectedCard[indexOfLastCard].get())
             ((self.player).selectedCard[indexOfLastCard]).set("")
 
+    #Finish turn button manager
     class finishTurn:
         def __init__(self,player):
             self.player = player
         def __call__(self):
+            #Get the number of card choose
             numberOfCard = 0
             for i in range(3):
                 if(len((self.player).selectedCard[i].get()) != 0):
                     numberOfCard +=1
+            #If there isn't the right number , it shows an error message and stop the process
             if(numberOfCard!=(self.player).whiteCardNumber):
                 showerror("","You should only select "+str((self.player).whiteCardNumber)+" card!")
                 return
+            #End the player turn
             (self.player).endTurn()
-            
+    
+    #Player constructor
     def __init__(self,name,gameWindows):
         self.name = name
         self.playing = BooleanVar()
@@ -144,7 +168,7 @@ class Player:
         self.playerFrame = Frame(gameWindows, borderwidth=2, relief=SUNKEN, padx=5, pady=5)
         Label(self.playerFrame,text="Turn of: "+str(name)).pack()
         
-        #Card Selected
+        #Choosed card frame
         selected_card_panel = LabelFrame(self.playerFrame, text="Your choice", padx=5, pady=5)
         selected_card_panel.pack(fill="both", expand="yes")
         
@@ -152,7 +176,7 @@ class Player:
         Label(selected_card_panel,textvariable=self.selectedCard[1]).pack()
         Label(selected_card_panel,textvariable=self.selectedCard[2]).pack()
         
-        #Card of the player
+        #Player deck frame
         card_panel = LabelFrame(self.playerFrame, text="Your cards")
         card_panel.pack(fill="both", expand="yes")
         
@@ -165,8 +189,10 @@ class Player:
         Button(buttonframe, text="Select", command=self.selectCard(self)).pack()
         Button(buttonframe, text="Unselect", command=self.unselectCard(self)).pack()
         
+        #End of turn button
         Button(self.playerFrame, text="Finish turn", command=self.finishTurn(self)).pack()
 
+    #To show and hide player deck on the main screen
     def pack(self):
         self.playerFrame.pack(expand=True)
     def unpack(self):
@@ -180,15 +206,15 @@ class Player:
         self.playing.set(False)
     def isPlaying(self):
         return self.playing
-        
-    def addWhiteCard(self,whitecard):
+    
+    def addWhiteCard(self,whitecard):       #Add whitecard to the player deck
         (self.card).insert(END,whitecard)
-    def setWhiteCardNumber(self,number):
+    def setWhiteCardNumber(self,number):    #Set the number of whitecard needed this turn
         self.whiteCardNumber = number
     
-    def getSelectedCard(self):
+    def getSelectedCard(self):              #Return the card choosed by the player
         return self.selectedCard
-    def getName(self):
+    def getName(self):                      #Return the player name
         return self.name
 
     def addPoint(self):
@@ -196,7 +222,11 @@ class Player:
     def getPoint(self):
         return self.point
  
+#Class that manage the czar
 class Czar:
+    """Choose button manager
+    This class add a point to the deck choose by the czar
+    """
     class Choose:
         def __init__(self,czar,players):
             self.czar = czar
@@ -205,6 +235,9 @@ class Czar:
             self.players[(self.czar.cardToJudge)[(self.czar).czarChoose.get()].getPlayerID()].addPoint()
             (self.czar).endTurn()
 
+    """
+    This class holds information about the players deck and enables to have anonymous deck for the czar choose
+    """
     class cardSelection:
         def __init__(self):
             self.playerID=-1
@@ -219,6 +252,7 @@ class Czar:
         def getPlayerCard(self):
             return self.playerCard  
 
+    #Czar constructor: build czar GUI
     def __init__(self,players,gameWindows):
         self.binding = []
         self.czarChoose = IntVar()
@@ -235,6 +269,7 @@ class Czar:
             Label(cardFrame,textvariable=(self.cardToJudge)[i].getPlayerCard()[2]).pack()
         Button(self.czarFrame, text="Choose", command=self.Choose(self,players)).pack()
     
+    #Shuffle the card choose by the player to be anonymous
     def shuffle(self,czarID,players):
         self.binding=[]
         for i in range(len(players)):
@@ -244,7 +279,7 @@ class Czar:
         for i in range(len(self.binding)):
             self.cardToJudge[i].setPlayerID(self.binding[i])
             self.cardToJudge[i].setPlayerCard(players[self.binding[i]].getSelectedCard())
-
+    
     def getWinnerID(self):
         return self.cardToJudge[self.czarChoose.get()].getPlayerID()
 
@@ -260,10 +295,12 @@ class Czar:
     def unpack(self):
         self.czarFrame.pack_forget()
     
+#Get a random black card and remove it from the list of available black card
 def getBlackCard(black):
     card = black.pop(rd.randint(0,len(black)-1))
     return (card.count("__"),card)
-    
+
+#Get a random white card to give to a player and remove it from the list of available white card
 def getWhiteCard(white,player,number):
     for i in range(number):
         player.addWhiteCard(white.pop(rd.randint(0,len(white)-1)))
@@ -275,6 +312,7 @@ def mainGameLoop(playersNameList,white_cards,black_cards):
     
     czar_id = 0
     winner_id = -1
+    #Windows and GUI management
     gameWindows = Toplevel(root)
     gameWindows.minsize(width=400, height=600)
     gameWindows.title("Cards Against Humanity : Game")
@@ -286,16 +324,19 @@ def mainGameLoop(playersNameList,white_cards,black_cards):
     showBlackText = StringVar()
     Label(gameWindows,textvariable=showBlackText).pack()
     
-    
     player_point = LabelFrame(gameWindows, text="Points:", padx=20, pady=20)
     player_point.pack(fill="both", expand="yes")
-    #Player generation
+    #---
+    
+    #Create a Player class for each player
     for i in range(len(playersNameList)):
         players.append(Player(playersNameList[i],gameWindows))
         getWhiteCard(game_white_cards,players[-1],5)
+        #Label to show players point
         Label(player_point,text=players[-1].getName()).grid(row=i, column=0)
         Label(player_point,textvariable=players[-1].getPoint()).grid(row=i, column=1)
-
+    
+    #Create the Czar class
     czar = Czar(players,gameWindows)
     while(winner_id == -1):
         showCzarText.set("Czar this turn: "+players[czar_id].getName())
@@ -303,7 +344,7 @@ def mainGameLoop(playersNameList,white_cards,black_cards):
         nb_white_to_withdraw,txt = getBlackCard(game_black_cards)
         showBlackText.set(txt)
         
-        #Player card selection
+        #Player card choose loop
         for i in range(len(players)):
             if(i==czar_id):
                 continue
@@ -321,17 +362,18 @@ def mainGameLoop(playersNameList,white_cards,black_cards):
         gameWindows.wait_variable(czar.isPlaying())
         czar.unpack()
         
+        #Check if the turn winner has win the game
         if(players[czar.getWinnerID()].getPoint().get()>5):
             winner_id = czar.getWinnerID()
-            
+        
+        #Change the Czar
         czar_id+=1
         if(czar_id>=len(players)):
             czar_id =0
         
     showInformation("","This game winner is "+players[winner_id]+"!")
-        
-## Global var / start function
 
+## Start function
 root = Tk()
 root.title("Cards Against Humanity, python v.0.1 by leaxs")
 root.minsize(width=200, height=400)
